e50ba3b4dee85349aae69663efc16f667c5e7b47
DannyFink1 <114002047+DannyFink1@users.noreply.github.com>
2023-03-06 12:13:46 +0100

    physic

--- src/at/htldornbirn/cwi/nawi/gamestates/parabola/Parabola.java
+++ src/at/htldornbirn/cwi/nawi/gamestates/parabola/Parabola.java
@@ -9,6 +9,15 @@ import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class Parabola extends BasicGameState {
+
+    private float platform_x, platform_y, newX, newY, gravitation, startVelocity, startAngle, height;
+    private float test;
+
+
+
+
+
+
     @Override
     public int getID() {
         return Constants.PARABOLA;
@@ -16,17 +25,38 @@ public class Parabola extends BasicGameState {
 
     @Override
     public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
-        // test
+        this.platform_x = 162f;
+        this.platform_y = 500f;
+        this.newX = 162f;
+        this.newY = 500f;
+        this.gravitation = 9.81f;
+        this.startVelocity = 1f;
+        this.startAngle = 30f;
+        this.height = 620-this.platform_y;
+        this.test = 0;
     }
 
     @Override
     public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics graphics) throws SlickException {
-        graphics.drawRect(50,50,50,50);
+        //Canvas
+        graphics.drawRect(50,50,1180,620);
+        //Platform
+        graphics.fillRect(50, this.platform_y, 125, 670-this.platform_y);
+        //Ball
+        graphics.fillOval(162,this.platform_y-25,25,25);
 
     }
 
     @Override
     public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int i) throws SlickException {
+        this.test++;
+        formula(this.test);
+        if(this.newY >= 0) {
+            System.out.println(this.newY);
+        }
+    }
 
+    public void formula(float posX) {
+        this.newY = (float) ((-1/2 * this.gravitation / Math.sqrt(this.startVelocity*Math.cos(this.startAngle)) * posX) + Math.tan(this.startAngle)*posX + this.height);
     }
 }
